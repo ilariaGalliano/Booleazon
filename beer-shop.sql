54265cc2b64c7bfe10b40616ed5625f5a37a112f
Pier Giulio Minetti <pg.minetti@gmail.com>
2021-01-29 16:53:40 +0100

    DB caricato

--- beer-shop.sql
+++ beer-shop.sql
@@ -1,14 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Jan 27, 2021 at 02:37 PM
--- Server version: 5.7.30
--- PHP Version: 7.4.9
+-- Host: 127.0.0.1:3306
+-- Creato il: Gen 29, 2021 alle 15:51
+-- Versione del server: 5.7.31
+-- Versione PHP: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `beer-shop`
@@ -17,11 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `beers`
+-- Struttura della tabella `beers`
 --
 
-CREATE TABLE `beers` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `beers`;
+CREATE TABLE IF NOT EXISTS `beers` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `type` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `origin` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -30,25 +38,19 @@ CREATE TABLE `beers` (
   `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `path_img` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- Dump dei dati per la tabella `beers`
 --
 
---
--- Indexes for table `beers`
---
-ALTER TABLE `beers`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `beers` (`id`, `title`, `type`, `origin`, `description`, `price`, `slug`, `path_img`, `created_at`, `updated_at`) VALUES
+(8, 'Heineken', 'Bionda', 'Olandese', 'Heineken è una birra lager al malto d\'orzo e luppolo, facile da bere e di gusto pulito. Versandola nel calice si presenta di un colore giallo oro, sormontato da una schiuma non troppo persistente. Al naso propone profumi vegetali, dominati dal luppolo.', 2.99, 'heineken', 'images/oVM477imXPFqsxl15BJsLkxAQpmEacZwHW6WJOjG.jpg', '2021-01-29 14:41:06', '2021-01-29 14:41:06'),
+(9, 'Tennent\'z', 'sdfsf', 'boh', 'Amara', 5.00, 'tennentz', NULL, '2021-01-29 14:47:14', '2021-01-29 14:47:14');
+COMMIT;
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `beers`
---
-ALTER TABLE `beers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
